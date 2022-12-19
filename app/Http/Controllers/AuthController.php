13ddc596ce90ff0fa4a75daa880378b87499eda2
MarkRywell <gopro855@gmail.com>
2022-12-19 21:23:56 +0800

    Updated Login and Register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,8 +10,15 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
+
     public function register(Request $request)
-    {
+    {   
+        $responseData = [
+            'status' => 'fail',
+            'message' => '',
+            'data' => null
+        ];
+
         $request->validate([
             'name' => 'required|string',
             'address' => 'required|string',
@@ -26,8 +33,52 @@ class AuthController extends Controller
             'password' => Hash::make($request->password)
         ]);
 
-        $user->save();
+        if ($user == null)
+        {   
+            $responseData['message'] = 'Unsuccessful Registration';
+            return response($responseData, 400);
+        }
 
-        return response()->json(['message' => 'User has been registered'], 201);
+        $user->save();
+        
+        $responseData['status'] = 'success';
+        $responseData['message'] = 'Successful Registration';
+        return response($responseData, 201);
+    }
+
+    public function login(Request $request)
+    {   
+        $responseData = [
+            'status' => 'fail',
+            'message' => '',
+            'data' => null,
+        ];
+
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string'
+        ]);
+
+        $credentials = request(['email', 'password']);
+
+        if(!Auth::attempt($credentials)) {
+            $responseData['message'] = 'Unsuccessful Login';
+
+            return response($responseData, 400);
+        }
+
+        $user = $request->user();
+    
+        $responseData = [
+            'status' => 'success',
+            'message' => 'Successful Login',
+            'data' => [
+                'user' => Auth::user(),
+                'token' => $user->createToken('onlysells')->plainTextToken
+            ]
+        ];
+
+        return response($responseData, 200);
+
     }
 }
